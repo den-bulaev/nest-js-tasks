5aea8d6f9c5e14cbdbfc6e026af6f4fbe571bbf7
den-bulaev <denisbulaev7@gmail.com>
2024-12-11 14:01:46 +0200

    signUp added

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 
 @Module({
